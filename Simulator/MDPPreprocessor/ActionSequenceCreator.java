12122d91b23f30338d253b7ff2e8a96484d7f62a
Beryl Ramadhian <berylramadhian@gmail.com>
2016-02-23 16:14:19 +0700

    added action sequences creator

---
+++ Simulator/MDPPreprocessor/ActionSequenceCreator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MDPPreprocessor;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Yorozuya Samurai
+ */
+public class ActionSequenceCreator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+    public ArrayList<String> createActionSequences(String[] action, int eachLoop, int totalLoop){
+        ArrayList<String> actSeq=new ArrayList();
+        
+        return actSeq;
+        
+        
+    }
+    
+}
